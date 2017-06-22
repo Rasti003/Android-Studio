d6497d02c10c2910e58da95ccdd20ffc875acda3
Rasti003@gmail.com <Rasti003@gmail.com>
2017-06-23 00:56:33 +0200

    Animal Sound - android play mp3 testing app

---
+++ AnimalSounds/app/src/main/java/com/example/przem/animalsounds/SoundButtonActivity.java
@@ -0,0 +1,29 @@
+package com.example.przem.animalsounds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
+
+public class SoundButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_button);
+
+        final MediaPlayer CatPlayer = MediaPlayer.create(this, R.raw.cat);
+
+        Button playCat = (Button) this.findViewById(R.id.button_cat);
+
+        playCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CatPlayer.start();
+
+
+            }
+        });
+    }
+}
