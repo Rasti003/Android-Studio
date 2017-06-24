d52f4f5aac105ddf11dc448589725aac7eecedc0
Rasti003@gmail.com <Rasti003@gmail.com>
2017-06-24 18:58:27 +0200

    -add new  GameActivity -Add a button to change the Activity -Add  button animation -Commenting code in Polish

--- AnimalSounds/app/src/main/java/com/example/przem/animalsounds/SoundButtonActivity.java
+++ AnimalSounds/app/src/main/java/com/example/przem/animalsounds/SoundButtonActivity.java
@@ -1,79 +1,116 @@
 package com.example.przem.animalsounds;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.Toast;
+
 
 public class SoundButtonActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_button);
 
-        final MediaPlayer CatPlayer = MediaPlayer.create(this, R.raw.cat);
+        final Animation animation =new AlphaAnimation(1.0f, 0.0f);
+        animation.setDuration(1000);
 
-        Button playCat = (Button) this.findViewById(R.id.button_cat);
+
+
+        final Button StartGame = (Button)findViewById(R.id.button_ActiveGame);  //ustawienie lisnera na przycisk o zadanym id (slucha OnClick)
+
+        StartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Game is started " , Toast.LENGTH_SHORT).show(); //Wyswietlanie "POP-UP" z napisem  dlaszy ci
+
+                    Intent intent = new Intent(getApplicationContext(), GameActivity.class); //PRZENOSZENIE NA INNE ACTIVITY: twozenie INTENS getApp..- aktualny kontekst aplikacje przycisk powrót tu przeniesie , Activity docelowe.class
+                startActivity(intent);                       //Wywołanie przeniesienia
+
+            }
+        });
+
+
+
+
+
+
+
+        final MediaPlayer CatPlayer = MediaPlayer.create(this, R.raw.cat);
+        final Button playCat = (Button) this.findViewById(R.id.button_cat);
 
         playCat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CatPlayer.start();
+                playCat.startAnimation(animation);
+
 
 
             }
         });
 
 
+
+
         final MediaPlayer DogPlayer = MediaPlayer.create(this, R.raw.dog);
 
-        Button playDog = (Button) this.findViewById(R.id.button_dog);
+       final Button playDog = (Button) this.findViewById(R.id.button_dog);
 
         playDog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DogPlayer.start();
-
+                playDog.startAnimation(animation);
 
             }
         });
 
         final MediaPlayer CowPlayer = MediaPlayer.create(this, R.raw.cow);
 
-        Button playCow = (Button) this.findViewById(R.id.button_cow);
+        final Button playCow = (Button) this.findViewById(R.id.button_cow);
 
         playCow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CowPlayer.start();
-
+                playCow.startAnimation(animation);
 
             }
         });
 
         final MediaPlayer MonkeyPlayer = MediaPlayer.create(this, R.raw.monkey);
 
-        Button playMonkey = (Button) this.findViewById(R.id.button_monkey);
+        final Button playMonkey = (Button) this.findViewById(R.id.button_monkey);
 
         playMonkey.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MonkeyPlayer.start();
-
+                playMonkey.startAnimation(animation);
 
             }
         });
 
         final MediaPlayer SheepPlayer = MediaPlayer.create(this, R.raw.sheep);
 
-        Button playSheep = (Button) this.findViewById(R.id.button_sheep);
+        final Button playSheep = (Button) this.findViewById(R.id.button_sheep);
 
         playSheep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SheepPlayer.start();
+                playSheep.startAnimation(animation);
 
 
             }
@@ -83,8 +120,8 @@ public class SoundButtonActivity extends AppCompatActivity {
 
 
 
-        }
 
+    }
 }
 
 
